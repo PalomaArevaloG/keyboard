737de038c8a92063af4761d4aab036611170a6be
Paloma <paloma.arevalog@gmail.com>
2021-12-06 12:47:21 +0100

    add function write key

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-console.log('>> Ready :)');
+let textarea = document.querySelector('.js-textarea');
+let key = document.querySelectorAll('.js-key');
+let delete = document.querySelector('.js-delete');
+let caps = document.querySelector('.js-caps');
+let space = document.querySelector('.js-space');
+
+
+for (let i = 0; i < key.length; i++) {
+	key[i].addEventListener('click', handleButtonClick);
+}
+function handleButtonClick(ev) {
+	console.log(ev.currentTarget.value);
+	textarea.value += ev.currentTarget.innerText;
+}
+
+/*
+//podría ser una opción, pero pone el inner text uniocamente y tengpo que seleccionar todo el div
+let textarea = document.querySelector('.js-textarea');
+document.querySelectorAll('.keyboard button').forEach((el) => {
+	el.addEventListener('click', () => {
+		textarea.value = textarea.value + el.innerText;
+	});
+}); */
